880965efbebaf82a450e7f6ae465b75dc246fca7
Iwillbeok2021 <1262625855@qq.com>
2022-08-06 19:47:23 +0800

    第一版

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+ * 根组件
+ */
+import React ,{Component} from "react";
+import { Button } from "antd";
+import 'antd/dist/antd.min.css'
+//路由器组件和路由组件  映射路由
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Login from "./pages/login/login";
+import Admin from "./pages/admin/admin";
+
+export default class App extends Component{
+
+  render(){
+
+    return(
+      <BrowserRouter>
+         <Routes>{/*只匹配一个**/}
+            <Route path="/login" element={<Login/>}> </Route>
+            <Route path="/" element={<Admin/>}> </Route>
+         </Routes>
+      </BrowserRouter>
+    )
+
+
+  }
+
 }
-
-export default App;
